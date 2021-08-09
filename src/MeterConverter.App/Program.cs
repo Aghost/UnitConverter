89116c9adf428ef3df371121731c35354ecc2966
Your Name <you@example.com>
2021-08-09 10:38:05 +0200

    init commit

---
+++ src/MeterConverter.App/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using static MeterConverter.Domain.UnitConverter;
+using static System.Console;
+
+string input = "";
+
+do {
+    WriteLine(ParseInput((input = ReadLine()), LengthUnits));
+
+} while (input != "exit");
---
+++ src/MeterConverter.Domain/UnitConverter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MeterConverter.Domain
+{
+    public class UnitConverter {
+        public static Dictionary<string, double> LengthUnits = new Dictionary<string, double> {
+            { "mm", 0.001 },
+            { "cm", 0.01 },
+            { "in", 0.0254 },
+            { "dm", 0.1 },
+            { "m", 1 },
+            { "km", 1000 }
+        };
+
+        public static Dictionary<string, double> MetricUnits = new Dictionary<string, double> {
+            {"tera", 1000000000000},
+            {"T", 1000000000000},
+            {"giga", 1000000000},
+            {"G", 1000000000},
+            {"mega", 1000000},
+            {"M", 1000000},
+            {"kilo", 1000},
+            {"k", 1000},
+            {"hecto", 100},
+            {"h", 100},
+            {"deca", 10},
+            {"da", 10},
+            {"none", 1},
+            {"deci", 0.1},
+            {"d", 0.1},
+            {"centi", 0.01},
+            {"c", 0.01},
+            {"milli", 0.001},
+            {"m", 0.001},
+            {"micro", 0.000001},
+            {"u", 0.000001},
+            {"nano", 0.000000001},
+            {"n", 0.000000001},
+            {"pico", 0.000000000001},
+            {"p", 0.000000000001},
+        };
+
+        public static string ParseInput(string str, Dictionary<string, double> dict) {
+            string[] values = str.Split(' ');
+
+            if ((values.Length == 3) && (Double.TryParse(values[0], out double val) && val > 0)) {
+                try {
+                    return $"{val} {values[1]} = {dict[values[1]] * (1 / dict[values[2]]) * val} {values[2]}";
+                } catch (Exception e) {
+                    return $"log: parsing-error\n{e}";
+                }
+            } else {
+                return values[0] == "exit" ? "exit..." : $"> Usage: value unit unit\n> units: {String.Join(' ', dict.Keys)}";
+            }
+        }
+    }
+}
