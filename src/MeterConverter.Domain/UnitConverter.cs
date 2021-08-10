4eb7479abcaa1c1075e98b0f4274ca9511ba9368
Aghost <youri_van_sorgen2@example.com>
2021-08-10 23:32:19 +0200

    removed unused

--- src/MeterConverter.Domain/UnitConverter.cs
+++ src/MeterConverter.Domain/UnitConverter.cs
@@ -52,17 +52,6 @@ namespace MeterConverter.Domain
             } catch (Exception e) {
                 return values[0] == "exit" ? "exit..." : $"ERROR: {e.ToString().Split('\n')[0]}\n> Usage: value unit unit\n> units: {String.Join(' ', dict.Keys)}";
             }
-
-            /*
-            try {
-                if ((values.Length == 3) && (Double.TryParse(values[0], out double val) && val > 0)) {
-                    return $"{val} {values[1]} = {Calculate(dict[values[1]], dict[values[2]], val)} {values[2]}";
-                }
-                return $"Invalid Input\n> Usage: VALUE UNIT UNIT\n> Units: {String.Join(' ', dict.Keys)}";
-            } catch (Exception e) {
-                return values[0] == "exit" ? "exit..." : $"ERROR: {e.ToString().Split('\n')[0]}\n> Usage: value unit unit\n> units: {String.Join(' ', dict.Keys)}"; 
-            }
-            */
         }
     }
 }
